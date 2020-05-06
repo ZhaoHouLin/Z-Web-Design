79fabe81f661a82b1338586808dcc0ea751bdf7b
RaidenLin <rodes5292@gmail.com>
2020-05-06 17:25:31 +0800

    Resume Swiper 樣式調整

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
 .resume
-  PreviousAndNext
+  //- PreviousAndNext
   h1(:style='fontColor') {{$route.name}}
-  .screen
-    .pages(:style='changePage')
+  swiper(ref='mySwiper' :options='swiperOptions')
+    .swiper-slide
       .education
         h2 education
         .info
@@ -15,27 +15,50 @@
             .period 2008 - 2012
             h3 國立台北科技大學
             h4 光電工程系
+    .swiper-slide
       .experience
         h2 experience
         .info
           .company1
-            .period 2013 - 20017
+            .period 2013 - 2017
             h3 翰碁有限公司
             h4 助理工程師
           .company2
             .period 2017 - Now
             h3 藍新資訊股份有限公司
             h4 疾病管制署 硬體維護人員
+    .swiper-pagination(slot='pagination')
+  //- .swiper-button-next(:style='fontColor')
+  //- .swiper-button-prev(:style='fontColor')
 </template>
 
 
 <script>
-
 import {mapGetters} from 'vuex'
 export default {
   data() {
     return {
-      
+      swiperOptions: {
+        effect: 'coverflow',
+        loop: true,
+        mousewheel: true,
+        coverflowEffect: {
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows : false,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        // navigation: {
+        //   nextEl: '.swiper-button-next',
+        //   prevEl: '.swiper-button-prev',
+        // },
+        // Some Swiper option/callback...
+      }
     }
   },
   methods: {
@@ -43,7 +66,12 @@ export default {
   },
   computed: {
     ...mapGetters(['fontColor','changePage']),
-    
+    swiper() {
+      return this.$refs.mySwiper.$swiper
+    }
+  },
+  mounted() {
+    this.swiper.slideTo(0, 1000, false)
   }
 }
 </script>
@@ -63,22 +91,14 @@ export default {
     right 24vh
     text-align center
     text-transform capitalize
-    
-  .screen
-    overflow hidden
-    white-space nowrap
-    size(480px,70%)
+  .swiper-container 
+    z-index 3
     flexCenter()
-    position relative
-    .pages
-      transition 0.5s
-      size(auto,auto)
+    size(600px,auto)
+    padding-bottom 36px
+    .swiper-slide
       flexCenter()
-      position absolute
-      .education,.experience
-        size(auto,auto)
-        h2
-          // text-shadow 8px 4px 8px rgba(0,0,0,0.5)
+      .education,.experience      
         .info
           flexCenter()
           .senior,.university,.company1,.company2
@@ -87,18 +107,26 @@ export default {
             h3,h4
               margin 8px 0
 
+.swiper-pagination
+  span 
+    background-color rgba(0,0,0,0.5)
+    size(10px)
+    transition all 0.3s
+  .swiper-pagination-bullet-active
+    transition all 0.3s
+    width 20px
+    border-radius 8px
+
 @media screen and (max-width: 1024px)
   .resume
-    flex-direction column
     h1
       font-size 4vh
       left 24vh
-    .screen
-      left 16px
-      top 16px
-      size(240px,70%)
-      .pages
+    .swiper-container 
+      .swiper-slide
         .education,.experience
+          transform scale(0.8)      
           .info
             flex-direction column
+
 </style>
