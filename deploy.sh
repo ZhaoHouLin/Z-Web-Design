507b9bde08eeda8e0df43666a00b4de452f5ef87
RaidenLin <rodes5292@gmail.com>
2020-06-04 14:27:19 +0800

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:ZhaoHouLin/ZhaoHouLin.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ZhaoHouLin/Z-Web-Design.git master:gh-pages
+git push -f https://github.com/ZhaoHouLin/Z-Web-Design.git master:gh-pages
 
 cd -
